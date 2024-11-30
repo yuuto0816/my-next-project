70debc0bd7e7bc8bda415649a65ea1cd51d7544a
yuuto0816 <ktc24a31g0019@edu.kyoto-tech.ac.jp>
2024-11-30 12:37:39 +0900

    p105 終了

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './index.module.css';
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <a href="/" className={styles.logoLink}>
+            <Link href="/" className={styles.logoLink}>
                 <Image
                     src="/logo.svg"
                     alt="SIMPLE"
@@ -13,11 +14,17 @@ export default function Header() {
                     height={133}
                     priority
                 />
-            </a>
-            <nav>
+            </Link>
+            <nav className={styles.nav}>
                 <ul className={styles.items}>
                     <li>
-                        <a href="?members">メンバー</a>
+                        <Link href="/news">ニュース</Link>
+                    </li>
+                    <li>
+                        <Link href="/members">メンバー</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">お問い合わせ</Link>
                     </li>
                 </ul>
             </nav>
