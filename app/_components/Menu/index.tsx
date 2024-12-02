a81536d82ed8183aa07cb4d841fee37e1e033593
yuuto0816 <167593083+yuuto0816@users.noreply.github.com>
2024-12-02 14:47:43 +0900

    p110 終了

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./index.module.css";
 
 export default function Menu() {
     return (
-        <nav className={styles.nav}>
-            <ul className={styles.items}>
-                <li>
-                    <Link href="/news">ニュース</Link>
-                </li>
-                <li>
-                    <Link href="/members">メンバー</Link>
-                </li>
-                <li>
-                    <Link href="/contact">お問い合わせ</Link>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <nav className={styles.nav}>
+                <ul className={styles.items}>
+                    <li>
+                        <Link href="/news">ニュース</Link>
+                    </li>
+                    <li>
+                        <Link href="/members">メンバー</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">お問い合わせ</Link>
+                    </li>
+                </ul>
+            </nav>
+            <button className={styles.button}>
+                <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
+            </button>
+        </div>
     );
 }
