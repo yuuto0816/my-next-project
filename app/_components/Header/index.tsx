8e94ea6f08c61929f625e75b3f01c41e0f46fac4
yuuto0816 <167593083+yuuto0816@users.noreply.github.com>
2024-12-02 16:18:39 +0900

    p116 終了

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -4,32 +4,19 @@ import styles from "./index.module.css";
 import Menu from "../Menu";
 
 export default function Header() {
-  return (
-    <header className={styles.header}>
-      <Link href="/" className={styles.logoLink}>
-        <Image
-          src="/logo.svg"
-          alt="SIMPLE"
-          className={styles.logo}
-          width={348}
-          height={133}
-          priority
-        />
-      </Link>
-      <Menu />
-      <nav className={styles.nav}>
-        <ul className={styles.items}>
-          <li>
-            <Link href="/news">ニュース</Link>
-          </li>
-          <li>
-            <Link href="/members">メンバー</Link>
-          </li>
-          <li>
-            <Link href="/contact">お問い合わせ</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <Link href="/" className={styles.logoLink}>
+                <Image
+                    src="/logo.svg"
+                    alt="SIMPLE"
+                    className={styles.logo}
+                    width={348}
+                    height={133}
+                    priority
+                />
+            </Link>
+            <Menu />
+        </header>
+    );
 }
