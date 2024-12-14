aeb96ad3e77c65ec4558ddf9983e9e5e80ff7527
yuuto0816 <167593083+yuuto0816@users.noreply.github.com>
2024-12-14 11:06:05 +0900

    p235 上部終了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -62,6 +62,11 @@ export const getNewsDetail = async (
     endpoint: "news",
     contentId,
     queries,
+    customRequestInit: {
+      next: {
+        revalidate: queries?.draftKey === undefined ? 60 : 0,
+      },
+    },
   });
   return detailData;
 };
